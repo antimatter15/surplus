404c8950b66a76a53425d58cc6a8cf7ed5aea01a
antimatter15 <antimatter15@gmail.com>
2011-07-01 14:52:00 -0400

    blah

--- notification.js
+++ notification.js
@@ -7,18 +7,3 @@ port.onMessage.addListener(function(msg){
     return [e.innerText, e.querySelector('img').src]
   }))
 })
-
-//console.log('SURPLUS NOTIFIER LOADED', location.href)
-
-/*
-window.addEventListener("beforeload", function(event) { 
-  console.log("Caught resource load", event.url); 
-  if(/notifications\/updatelastreadtimeevent/.test(event.url)){
-    //we know this is a update read time event, and we can kill it with fire.
-    console.log('killed with fire');
-    event.preventDefault();
-  }
-}, true); 
-*/
-
-
