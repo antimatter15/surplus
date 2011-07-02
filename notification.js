18d00fa66f46efc8c83761a3ee2df923648aea88
antimatter15 <antimatter15@gmail.com>
2011-07-02 16:56:03 -0400

    fixed xdomain issues

--- notification.js
+++ notification.js
@@ -2,8 +2,18 @@ var port = chrome.extension.connect({name: location.href});
 
 port.onMessage.addListener(function(msg){
   console.log('recieved query for notifications')
-  var divs = document.querySelectorAll('#summary-content>div');
-  port.postMessage([].slice.call(divs, 0, msg.num).map(function(e){
-    return [e.innerText, e.querySelector('img').src]
-  }))
+  if(msg.action == 'notifications'){
+    var divs = document.querySelectorAll('#summary-content>div');
+    port.postMessage({action: 'notifications', messages: [].slice.call(divs, 0, msg.num).map(function(e){
+      return [e.innerText, e.querySelector('img').src]
+    })})
+  }else if(msg.action == 'xhr'){
+    var xhr = new XMLHttpRequest();
+    xhr.open('get', msg.url, true);
+    xhr.onload = function(){
+      port.postMessage({action: 'xhr', response: xhr.responseText})
+    }
+    xhr.send();
+  }
+  
 })
