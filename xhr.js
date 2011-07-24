716577d96c30e82e76310c55fdecedf8b6e4cd35
antimatter15 <antimatter15@gmail.com>
2011-07-24 15:19:49 -0400

    This doesn't work.
    Retain unread-ed-ness.

--- xhr.js
+++ xhr.js
@@ -7,9 +7,29 @@
   "all_frames": true
 }
 */
+var sharedElement = document.createElement('div');
+sharedElement.style.display = 'none';
+sharedElement.id = 'SurplusSharedElement'
+document.documentElement.appendChild(sharedElement);
+
+var customEvent = document.createEvent('Event');
+customEvent.initEvent('surplusXHRResponse', true, true);
+
 var script = document.createElement('script');
 script.type = 'text/javascript';
 script.innerHTML = ';('+(function(){
+  var callbacks = {};
+  
+  var sharedElement = document.getElementById('SurplusSharedElement');
+  sharedElement.addEventListener('surplusXHRResponse', function(){
+    var s = JSON.parse(sharedElement.innerText);
+    console.log(s);
+    callbacks[s[0]](s[1]);
+  })
+  var customEvent = document.createEvent('Event');
+  customEvent.initEvent('surplusXHRFound', true, true);
+  
+  
   XMLHttpRequest.prototype.__open = XMLHttpRequest.prototype.open;
   XMLHttpRequest.prototype.__send = XMLHttpRequest.prototype.send;
   XMLHttpRequest.prototype.open = function(type, url, async){
@@ -18,12 +38,27 @@ script.innerHTML = ';('+(function(){
   }
   XMLHttpRequest.prototype.send = function(data){
     if(/updatelastreadtime/.test(this.__url)){
-      chrome.extension.sendRequest({action: 'readtime', url: this.__url}, function(response) {
-        if(response == true) this.__send(data);        
-      });
+      var xhr = this;
+      var cbstr = Math.random().toString(36).substr(3);
+      callbacks[cbstr] = function(result){
+        if(result == true){
+          xhr.__send(data);
+        }
+      }
+      sharedElement.innerText = cbstr;
+      sharedElement.dispatchEvent(customEvent);
     }else{
       this.__send(data)
     }
   }
 }).toString()+')();'
 document.documentElement.appendChild(script);
+
+
+sharedElement.addEventListener('surplusXHRFound', function(){
+  var cbstr = sharedElement.innerText;
+  chrome.extension.sendRequest({action: 'readtime', url: this.__url}, function(response) {
+    sharedElement.innerText = JSON.stringify([cbstr, response]);
+    sharedElement.dispatchEvent(customEvent);
+  })
+})
