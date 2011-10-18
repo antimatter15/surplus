5d7da86b7785c3420ee8a4449045cc9b03688d4b
antimatter15 <antimatter15@gmail.com>
2011-10-18 19:03:06 -0300

    trimmed it down

--- testing/lite.js
+++ testing/lite.js
@@ -13,11 +13,7 @@ function showSharebox(){
 }
 
 function showNotifications(){
-    frames[0].postMessage('{"s":"onHoverNotifications","f":"..","c":0,"a":[""],"t":"86208861","l":false,"g":true,"r":".."}', 'https://plus.google.com')
-    setTimeout(function(){
-        frames[0].postMessage('{"s":"onShowNotificationsOnly","f":"..","c":0,"a":["",{"maxWidgetHeight":311}],"t":"86208861","l":false,"g":true,"r":".."}', 'https://plus.google.com')
-
-    },10000)
+    frames[0].postMessage('{"s":"onShowNotificationsOnly","f":"..","c":0,"a":["",{"maxWidgetHeight":311}],"t":"86208861","l":false,"g":true,"r":".."}', 'https://plus.google.com')
 }
 
 function hideWidgets(){
@@ -26,23 +22,11 @@ function hideWidgets(){
 
 var container = document.createElement('div');
 container.style.width = '440px';
-container.style.height = '300px';
 container.style.overflow = 'hidden';
 container.style.position = 'absolute';
-container.style.top = '1px';
-container.style.right = '4px';
-container.style.border = '1px solid gray';
+
 var iframe = document.createElement('iframe');
 iframe.scrolling = 'no';
-iframe.setAttribute('allowtransparency','true');
-iframe.setAttribute('hspace','0')
-iframe.setAttribute('id', 'gbsf');
-iframe.setAttribute('marginheight', '0');
-iframe.setAttribute('marginwidth', '0');
-iframe.setAttribute('name', 'gbsf');
-iframe.setAttribute('vspace', '0');
-iframe.setAttribute('tabindex', '-1');
-iframe.setAttribute('title', 'Notifications');
 
 iframe.width = '100%'; //eek it feels like a sin to write this
 iframe.frameBorder = 'no';
@@ -56,33 +40,17 @@ onmessage = function(e){
     }else if(j.s == '_resizeMe'){
         iframe.style.height = j.a[1].height+'px';
     }else if(j.s == '_ready'){
-            frames[0].postMessage('{"s":"__cb","f":"..","c":1,"a":[1,null],"t":"86208861","l":false,"g":true,"r":".."}', 'https://plus.google.com');
-
-setTimeout(function(){
-            frames[0].postMessage('{"s":"getVarc","f":"..","c":2,"a":["","base"],"t":"86208861","l":false,"g":true,"r":".."}', 'https://plus.google.com')
-        
-        }, 1000)
-        //showNotifications();
+        frames[0].postMessage('{"s":"__cb","f":"..","c":1,"a":[1,null],"t":"86208861","l":false,"g":true,"r":".."}', 'https://plus.google.com');
+        frames[0].postMessage('{"s":"getVarc","f":"..","c":2,"a":["","base"],"t":"86208861","l":false,"g":true,"r":".."}', 'https://plus.google.com')
+        showNotifications();
     }else if(j.s == 'navigateTo'){
         console.log(j.a[1].url); //this is the magical command to open a tab
     }else if(j.s == 'hideNotificationWidget'){
         //then reopen the default.
     }
     console.log(j.s,e)
-    
 }
 container.appendChild(iframe)
 document.body.appendChild(container)
 
-var blah = document.createElement('div');
-blah.style.position = 'absolute'
-blah.style.top = '500px';
-blah.innerHTML = '<h1>all hail le almighty goog</h1><p>i am cow</p><p>rawr ima dinosawr</p>'
-document.body.appendChild(blah);
-//ha ha, escape methods....
-//var houdini = escape("onError,onInfo,hideNotificationWidget,postSharedMessage,setNotificationWidgetHeight,switchTo,navigateTo,setNotificationText,handlePosted,_ready,_close,_open,_resizeMe");
-//var houdini = escape("onError,onInfo,hideNotificationWidget,postSharedMessage,setNotificationWidgetHeight,switchTo,navigateTo,setNotificationText,handlePosted,_ready,_close,_open,_resizeMe");
-
 iframe.src = 'https://plus.google.com/u/0/_/notifications/frame?hl=en&origin=http%3A%2F%2Fwww.google.com&jsh=r%3Bgc%2F23980661-3686120e#pid=1&id=gbsf&parent=http%3A%2F%2Fwww.google.com&rpctoken=86208861&_methods=onError%2ConInfo%2ChideNotificationWidget%2CpostSharedMessage%2CsetNotificationWidgetHeight%2CswitchTo%2CnavigateTo%2CsetNotificationText%2ChandlePosted%2C_ready%2C_close%2C_open%2C_resizeMe'
-
-//'https://plus.google.com/u/0/_/notifications/frame?hl=en&origin=http%3A%2F%2Fwww.google.com#pid=119&id=gbsf&parent=http%3A%2F%2Fwww.google.com&rpctoken=2718281828&_methods='+houdini;
